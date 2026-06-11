64163be09d7a64383cef66db4a5083701560a8c2
Vadim Skipin <vadim.skipin@clickhouse.com>
2026-06-11 10:00:18 +0000

    Revert "Migrate to bundled fcontext code"
    This reverts commit b1036d796b5be0bda65adc1462cb57047496b312.

--- src/fibers/fiber.cpp
+++ src/fibers/fiber.cpp
@@ -16,6 +16,8 @@
 #include <silk/util/stack.h>
 #include <silk/util/tsc.h>
 
+#include <boost/context/detail/fcontext.hpp>
+
 #include <algorithm>
 #include <atomic>
 #include <cerrno>
@@ -26,7 +28,6 @@
 #include <thread>
 #include <utility>
 
-#include <fcontext.h>
 #include <liburing.h>
 #include <poll.h>
 #include <pthread.h>
@@ -127,7 +128,7 @@ public:
     void parkThread() noexcept;
 
     // Fiber entry point.  Called once when the fiber is first activated.
-    [[noreturn]] static void fiberContextMain(transfer_t transfer) noexcept;
+    static void fiberContextMain(boost::context::detail::transfer_t transfer) noexcept;
 
     // Cache line 0: scheduling + per-suspend hot path. Touched on every
     // dispatch and every suspension. runFiber's full read/write set lives on
@@ -175,10 +176,10 @@ public:
     // waitingFuture) piggyback for free.
     struct alignas(CACHELINE_SIZE)
     {
-        // mmap'd stack and fcontext handles for cooperative switching.
+        // mmap'd stack and Boost.Context fcontext handles for cooperative switching.
         void * stack = nullptr;
-        fcontext_t fiberContext = nullptr;
-        fcontext_t threadContext = nullptr;
+        boost::context::detail::fcontext_t fiberContext = nullptr;
+        boost::context::detail::fcontext_t threadContext = nullptr;
 
         // Entry point and optional parameters destructor. parametersDtor is set
         // by run for non-trivially-destructible T and called by
@@ -317,7 +318,8 @@ bool Fiber::initialize(
 
     fiberMain = fiberMain_;
     parametersDtor = parametersDtor_;
-    fiberContext = make_fcontext(static_cast<uint8_t *>(stack) + PAGE_SIZE + fiberStackSize, fiberStackSize, fiberContextMain);
+    fiberContext = boost::context::detail::make_fcontext(
+        static_cast<uint8_t *>(stack) + PAGE_SIZE + fiberStackSize, fiberStackSize, fiberContextMain);
 
     return true;
 }
@@ -350,7 +352,7 @@ void Fiber::switchToFiberContext() noexcept
     TSAN_FIBER_SWITCH(tsanFiber);
 #endif
 
-    auto transfer = jump_fcontext(fiberContext, this);
+    auto transfer = boost::context::detail::jump_fcontext(fiberContext, this);
     fiberContext = transfer.fctx;
 
 #if defined(__SANITIZE_ADDRESS__)
@@ -370,7 +372,7 @@ void Fiber::switchToThreadContext(bool final) noexcept
     TSAN_FIBER_SWITCH(tsanSchedulerFiber);
 #endif
 
-    auto transfer = jump_fcontext(threadContext, nullptr);
+    auto transfer = boost::context::detail::jump_fcontext(threadContext, nullptr);
     threadContext = transfer.fctx;
 
 #if defined(__SANITIZE_ADDRESS__)
@@ -380,9 +382,9 @@ void Fiber::switchToThreadContext(bool final) noexcept
 #endif
 }
 
-void Fiber::fiberContextMain(transfer_t transfer) noexcept
+void Fiber::fiberContextMain(boost::context::detail::transfer_t transfer) noexcept
 {
-    // transfer is populated by uninstrumented assembly code (jump_fcontext).
+    // transfer is populated by Boost.Context's uninstrumented assembly code.
     // MSan cannot see those writes, so mark the struct as initialized here.
     MSAN_UNPOISON(&transfer, sizeof(transfer));
 
